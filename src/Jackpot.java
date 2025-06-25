a6240084c281b83d84cee8beb388786f7d5ef4be
Kiran <sudhaniure2@gmail.com>
2025-06-25 18:22:42 +0545

    constructor for question and options

---
+++ src/Jackpot.java
@@ -0,0 +1,16 @@
+public class Jackpot {
+    private int title;
+    private int option_1;
+    private int option_2;
+    private int option_3;
+    private int option_4;
+    private int checkAnswer;
+    public Jackpot(int title,int option_1,int option_2,int option_3,int option_4,int checkAnswer ){
+        this.title=title;
+        this.option_1=option_1;
+        this.option_2=option_2;
+        this.option_3=option_3;
+        this.option_4=option_4;
+        this.checkAnswer= checkAnswer;
+    }
+}
